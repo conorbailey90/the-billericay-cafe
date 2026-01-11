6a1bbd3cff1117c55711a5226ddf650c1e424b51
Conor Bailey <conbailey90@gmail.com>
2026-01-11 20:52:22 +0000

    link error fixed

--- svelte.config.js
+++ svelte.config.js
@@ -7,16 +7,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
-	adapter: adapter({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: 'build',
-		assets: 'build',
-		fallback: undefined,
-		precompress: false,
-		strict: true
-	})
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
